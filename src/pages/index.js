80348cf622aa32a7e94de848fadbcb06819aac5f
Jaume Romero Naranjo <jaumern51@gmail.com>
2023-11-16 11:45:41 +0000

    fixed website front end

--- src/pages/index.js
+++ src/pages/index.js
@@ -22,11 +22,7 @@ function Home() {
       <div class="row">
       <div class="column">
       <div class="card"> 
-      <img
-        src="https://attachments.office.net/owa/jromeronaranjo%40stu.acs-schools.com/service.svc/s/GetAttachmentThumbnail?id=AQMkADdiYzU2NGExLWE5ODktNDFmMS05MjM3LTNkNDhmYzY4ZjFjYQBGAAAD2MdSUUL9ukiIY7OrKpo2xgcAQP4AhPDRhEmqRdWrl4UHrQAAAgEMAAAAQP4AhPDRhEmqRdWrl4UHrQACFXrbEAAAAAESABAAmVUAG7XfWUqbUyKspvfdUw%3D%3D&thumbnailType=2&token=eyJhbGciOiJSUzI1NiIsImtpZCI6IjczRkI5QkJFRjYzNjc4RDRGN0U4NEI0NDBCQUJCMTJBMzM5RDlGOTgiLCJ0eXAiOiJKV1QiLCJ4NXQiOiJjX3VidnZZMmVOVDM2RXRFQzZ1eEtqT2RuNWcifQ.eyJvcmlnaW4iOiJodHRwczovL291dGxvb2sub2ZmaWNlLmNvbSIsInVjIjoiY2Y1YTY2MWI0Y2YzNGEzNWE3ODc2NzY5Mzc5MWYxOWUiLCJzaWduaW5fc3RhdGUiOiJbXCJrbXNpXCJdIiwidmVyIjoiRXhjaGFuZ2UuQ2FsbGJhY2suVjEiLCJhcHBjdHhzZW5kZXIiOiJPd2FEb3dubG9hZEBiNDZkODFkMS1hMmUzLTQ5NjktOWRlZC04MTE1NDUyOTA4YjIiLCJpc3NyaW5nIjoiV1ciLCJhcHBjdHgiOiJ7XCJtc2V4Y2hwcm90XCI6XCJvd2FcIixcInB1aWRcIjpcIjExNTM4MDExMjAxNjIxNTU0MjdcIixcInNjb3BlXCI6XCJPd2FEb3dubG9hZFwiLFwib2lkXCI6XCJhNmZhNzczYS1lOTRjLTRhZDQtYTRmYy0yOTg4MjBmNTVjM2JcIixcInByaW1hcnlzaWRcIjpcIlMtMS01LTIxLTI0Mjc1MTU1NDktODQ0ODgzODgtMzY5OTAxNTgzNy0yNzkzMzQ1NlwifSIsIm5iZiI6MTcwMDA4MTk0NSwiZXhwIjoxNzAwMDgyNTQ1LCJpc3MiOiIwMDAwMDAwMi0wMDAwLTBmZjEtY2UwMC0wMDAwMDAwMDAwMDBAYjQ2ZDgxZDEtYTJlMy00OTY5LTlkZWQtODExNTQ1MjkwOGIyIiwiYXVkIjoiMDAwMDAwMDItMDAwMC0wZmYxLWNlMDAtMDAwMDAwMDAwMDAwL2F0dGFjaG1lbnRzLm9mZmljZS5uZXRAYjQ2ZDgxZDEtYTJlMy00OTY5LTlkZWQtODExNTQ1MjkwOGIyIiwiaGFwcCI6Im93YSJ9.KZlMI8TiTGEmMQU2i51j9ZPGrFw2oEtjwpu-cG_leOpbwugDedCG7LyDRVA-piA_0ac_rSPUkGeBnwjNlNAWuQp9_-txA6qZE3voZ29dZqebspcVg5mOO6DWFg_RacM-XBdhKH5ZTU_AP3JkQZkiwfdEKqCR_WFORYPM0vZQp-wXR2cJlz3oZTIeTmQmHeJHcsfCkWqaVXW7W0ryGfPlWTwqvpP93IS6OhzFfsIokKJHHqwPMn0gMtTlExv4hbBARj61lu06ERPSn9q_JceIWYbCtV-S3lRi-vlrr5QP86PL5R1vj2AmpKZwuqNOJIgU_E1MN-STeNVm7jgQBJysIg&X-OWA-CANARY=P_hFfOnsC0aUmSmYG1kDRUDIY80d5tsYGArm6hByXOQTaxYd2QskZA8SEgTylRa0e3C-FnESo3s.&owa=outlook.office.com&scriptVer=20231103003.19&animation=true"
-        alt="Dhairya"
-        class="profile"
-      />
+      <iframe src="https://drive.google.com/file/d/1sxlOQxq39ONdQiwct0_T5jlo6erLMBPU/preview" class="profile" width="480" height="384px" allow="autoplay"></iframe>
       <div class="container">
         <h2 class="name">Dhairya</h2>
         <p class="title">Programmer &amp; Co-Founder</p>
@@ -40,11 +36,8 @@ function Home() {
 
       <div class="column">
       <div class="card"> 
-      <img
-        src="https://attachments.office.net/owa/jromeronaranjo%40stu.acs-schools.com/service.svc/s/GetAttachmentThumbnail?id=AQMkADdiYzU2NGExLWE5ODktNDFmMS05MjM3LTNkNDhmYzY4ZjFjYQBGAAAD2MdSUUL9ukiIY7OrKpo2xgcAQP4AhPDRhEmqRdWrl4UHrQAAAgEMAAAAQP4AhPDRhEmqRdWrl4UHrQACFXrbEAAAAAESABAAnfco2vG%2Bv0eZhRGaMNDfPA%3D%3D&thumbnailType=2&token=eyJhbGciOiJSUzI1NiIsImtpZCI6IjczRkI5QkJFRjYzNjc4RDRGN0U4NEI0NDBCQUJCMTJBMzM5RDlGOTgiLCJ0eXAiOiJKV1QiLCJ4NXQiOiJjX3VidnZZMmVOVDM2RXRFQzZ1eEtqT2RuNWcifQ.eyJvcmlnaW4iOiJodHRwczovL291dGxvb2sub2ZmaWNlLmNvbSIsInVjIjoiY2Y1YTY2MWI0Y2YzNGEzNWE3ODc2NzY5Mzc5MWYxOWUiLCJzaWduaW5fc3RhdGUiOiJbXCJrbXNpXCJdIiwidmVyIjoiRXhjaGFuZ2UuQ2FsbGJhY2suVjEiLCJhcHBjdHhzZW5kZXIiOiJPd2FEb3dubG9hZEBiNDZkODFkMS1hMmUzLTQ5NjktOWRlZC04MTE1NDUyOTA4YjIiLCJpc3NyaW5nIjoiV1ciLCJhcHBjdHgiOiJ7XCJtc2V4Y2hwcm90XCI6XCJvd2FcIixcInB1aWRcIjpcIjExNTM4MDExMjAxNjIxNTU0MjdcIixcInNjb3BlXCI6XCJPd2FEb3dubG9hZFwiLFwib2lkXCI6XCJhNmZhNzczYS1lOTRjLTRhZDQtYTRmYy0yOTg4MjBmNTVjM2JcIixcInByaW1hcnlzaWRcIjpcIlMtMS01LTIxLTI0Mjc1MTU1NDktODQ0ODgzODgtMzY5OTAxNTgzNy0yNzkzMzQ1NlwifSIsIm5iZiI6MTcwMDA4MTk0NSwiZXhwIjoxNzAwMDgyNTQ1LCJpc3MiOiIwMDAwMDAwMi0wMDAwLTBmZjEtY2UwMC0wMDAwMDAwMDAwMDBAYjQ2ZDgxZDEtYTJlMy00OTY5LTlkZWQtODExNTQ1MjkwOGIyIiwiYXVkIjoiMDAwMDAwMDItMDAwMC0wZmYxLWNlMDAtMDAwMDAwMDAwMDAwL2F0dGFjaG1lbnRzLm9mZmljZS5uZXRAYjQ2ZDgxZDEtYTJlMy00OTY5LTlkZWQtODExNTQ1MjkwOGIyIiwiaGFwcCI6Im93YSJ9.KZlMI8TiTGEmMQU2i51j9ZPGrFw2oEtjwpu-cG_leOpbwugDedCG7LyDRVA-piA_0ac_rSPUkGeBnwjNlNAWuQp9_-txA6qZE3voZ29dZqebspcVg5mOO6DWFg_RacM-XBdhKH5ZTU_AP3JkQZkiwfdEKqCR_WFORYPM0vZQp-wXR2cJlz3oZTIeTmQmHeJHcsfCkWqaVXW7W0ryGfPlWTwqvpP93IS6OhzFfsIokKJHHqwPMn0gMtTlExv4hbBARj61lu06ERPSn9q_JceIWYbCtV-S3lRi-vlrr5QP86PL5R1vj2AmpKZwuqNOJIgU_E1MN-STeNVm7jgQBJysIg&X-OWA-CANARY=hVKik6cjV06kvtBIP8vS9sDGKM8d5tsY_eaSkebj9ZmMjYwxyTWONkURWKK9_mjHsH3AcUCB43w.&owa=outlook.office.com&scriptVer=20231103003.19&animation=true"
-        alt="Emir"
-        class="profile"
-      />
+
+      <iframe src="https://drive.google.com/file/d/1Eak-pfF6NyOgaxtE18HZKqIoIhfz0H5G/preview" class="profile" width="480" height="384px" allow="autoplay"></iframe>
       <div class="container">
         <h2 class="name">Emir</h2>
         <p class="title">Programmer &amp; Co-Founder</p>
@@ -57,11 +50,7 @@ function Home() {
 
       <div class="column">
       <div class="card"> 
-      <img
-        src="https://attachments.office.net/owa/jromeronaranjo%40stu.acs-schools.com/service.svc/s/GetAttachmentThumbnail?id=AQMkADdiYzU2NGExLWE5ODktNDFmMS05MjM3LTNkNDhmYzY4ZjFjYQBGAAAD2MdSUUL9ukiIY7OrKpo2xgcAQP4AhPDRhEmqRdWrl4UHrQAAAgEMAAAAQP4AhPDRhEmqRdWrl4UHrQACFXrbEAAAAAESABAAglnzSAA8NkKD8XdofD8Yrg%3D%3D&thumbnailType=2&token=eyJhbGciOiJSUzI1NiIsImtpZCI6IjczRkI5QkJFRjYzNjc4RDRGN0U4NEI0NDBCQUJCMTJBMzM5RDlGOTgiLCJ0eXAiOiJKV1QiLCJ4NXQiOiJjX3VidnZZMmVOVDM2RXRFQzZ1eEtqT2RuNWcifQ.eyJvcmlnaW4iOiJodHRwczovL291dGxvb2sub2ZmaWNlLmNvbSIsInVjIjoiY2Y1YTY2MWI0Y2YzNGEzNWE3ODc2NzY5Mzc5MWYxOWUiLCJzaWduaW5fc3RhdGUiOiJbXCJrbXNpXCJdIiwidmVyIjoiRXhjaGFuZ2UuQ2FsbGJhY2suVjEiLCJhcHBjdHhzZW5kZXIiOiJPd2FEb3dubG9hZEBiNDZkODFkMS1hMmUzLTQ5NjktOWRlZC04MTE1NDUyOTA4YjIiLCJpc3NyaW5nIjoiV1ciLCJhcHBjdHgiOiJ7XCJtc2V4Y2hwcm90XCI6XCJvd2FcIixcInB1aWRcIjpcIjExNTM4MDExMjAxNjIxNTU0MjdcIixcInNjb3BlXCI6XCJPd2FEb3dubG9hZFwiLFwib2lkXCI6XCJhNmZhNzczYS1lOTRjLTRhZDQtYTRmYy0yOTg4MjBmNTVjM2JcIixcInByaW1hcnlzaWRcIjpcIlMtMS01LTIxLTI0Mjc1MTU1NDktODQ0ODgzODgtMzY5OTAxNTgzNy0yNzkzMzQ1NlwifSIsIm5iZiI6MTcwMDA4MTk0NSwiZXhwIjoxNzAwMDgyNTQ1LCJpc3MiOiIwMDAwMDAwMi0wMDAwLTBmZjEtY2UwMC0wMDAwMDAwMDAwMDBAYjQ2ZDgxZDEtYTJlMy00OTY5LTlkZWQtODExNTQ1MjkwOGIyIiwiYXVkIjoiMDAwMDAwMDItMDAwMC0wZmYxLWNlMDAtMDAwMDAwMDAwMDAwL2F0dGFjaG1lbnRzLm9mZmljZS5uZXRAYjQ2ZDgxZDEtYTJlMy00OTY5LTlkZWQtODExNTQ1MjkwOGIyIiwiaGFwcCI6Im93YSJ9.KZlMI8TiTGEmMQU2i51j9ZPGrFw2oEtjwpu-cG_leOpbwugDedCG7LyDRVA-piA_0ac_rSPUkGeBnwjNlNAWuQp9_-txA6qZE3voZ29dZqebspcVg5mOO6DWFg_RacM-XBdhKH5ZTU_AP3JkQZkiwfdEKqCR_WFORYPM0vZQp-wXR2cJlz3oZTIeTmQmHeJHcsfCkWqaVXW7W0ryGfPlWTwqvpP93IS6OhzFfsIokKJHHqwPMn0gMtTlExv4hbBARj61lu06ERPSn9q_JceIWYbCtV-S3lRi-vlrr5QP86PL5R1vj2AmpKZwuqNOJIgU_E1MN-STeNVm7jgQBJysIg&X-OWA-CANARY=txPyjIj-l0yFi_bQxr_yo1CMrPEd5tsYu-jmTVj6TyAWjJyyPHJKNaYr5Z9p-uQavkiMXh-reE0.&owa=outlook.office.com&scriptVer=20231103003.19&animation=true"
-        alt="Shanay"
-        class="profile"
-      />
+      <iframe src="https://drive.google.com/file/d/1D-ekF2s4HKTaN9mELFHtui3cBbzUH9BB/preview" class="profile" width="480" height="384px" allow="autoplay" ></iframe>
       <div class="container">
         <h2 class="name">Shanay</h2>
         <p class="title">Programmer &amp; Co-Founder</p>
@@ -78,11 +67,7 @@ function Home() {
 
       <div class="column">
       <div class="card"> 
-      <img
-        src="https://attachments.office.net/owa/jromeronaranjo%40stu.acs-schools.com/service.svc/s/GetAttachmentThumbnail?id=AQMkADdiYzU2NGExLWE5ODktNDFmMS05MjM3LTNkNDhmYzY4ZjFjYQBGAAAD2MdSUUL9ukiIY7OrKpo2xgcAQP4AhPDRhEmqRdWrl4UHrQAAAgEMAAAAQP4AhPDRhEmqRdWrl4UHrQACFXrbEAAAAAESABAAglnzSAA8NkKD8XdofD8Yrg%3D%3D&thumbnailType=2&token=eyJhbGciOiJSUzI1NiIsImtpZCI6IjczRkI5QkJFRjYzNjc4RDRGN0U4NEI0NDBCQUJCMTJBMzM5RDlGOTgiLCJ0eXAiOiJKV1QiLCJ4NXQiOiJjX3VidnZZMmVOVDM2RXRFQzZ1eEtqT2RuNWcifQ.eyJvcmlnaW4iOiJodHRwczovL291dGxvb2sub2ZmaWNlLmNvbSIsInVjIjoiY2Y1YTY2MWI0Y2YzNGEzNWE3ODc2NzY5Mzc5MWYxOWUiLCJzaWduaW5fc3RhdGUiOiJbXCJrbXNpXCJdIiwidmVyIjoiRXhjaGFuZ2UuQ2FsbGJhY2suVjEiLCJhcHBjdHhzZW5kZXIiOiJPd2FEb3dubG9hZEBiNDZkODFkMS1hMmUzLTQ5NjktOWRlZC04MTE1NDUyOTA4YjIiLCJpc3NyaW5nIjoiV1ciLCJhcHBjdHgiOiJ7XCJtc2V4Y2hwcm90XCI6XCJvd2FcIixcInB1aWRcIjpcIjExNTM4MDExMjAxNjIxNTU0MjdcIixcInNjb3BlXCI6XCJPd2FEb3dubG9hZFwiLFwib2lkXCI6XCJhNmZhNzczYS1lOTRjLTRhZDQtYTRmYy0yOTg4MjBmNTVjM2JcIixcInByaW1hcnlzaWRcIjpcIlMtMS01LTIxLTI0Mjc1MTU1NDktODQ0ODgzODgtMzY5OTAxNTgzNy0yNzkzMzQ1NlwifSIsIm5iZiI6MTcwMDA4MTk0NSwiZXhwIjoxNzAwMDgyNTQ1LCJpc3MiOiIwMDAwMDAwMi0wMDAwLTBmZjEtY2UwMC0wMDAwMDAwMDAwMDBAYjQ2ZDgxZDEtYTJlMy00OTY5LTlkZWQtODExNTQ1MjkwOGIyIiwiYXVkIjoiMDAwMDAwMDItMDAwMC0wZmYxLWNlMDAtMDAwMDAwMDAwMDAwL2F0dGFjaG1lbnRzLm9mZmljZS5uZXRAYjQ2ZDgxZDEtYTJlMy00OTY5LTlkZWQtODExNTQ1MjkwOGIyIiwiaGFwcCI6Im93YSJ9.KZlMI8TiTGEmMQU2i51j9ZPGrFw2oEtjwpu-cG_leOpbwugDedCG7LyDRVA-piA_0ac_rSPUkGeBnwjNlNAWuQp9_-txA6qZE3voZ29dZqebspcVg5mOO6DWFg_RacM-XBdhKH5ZTU_AP3JkQZkiwfdEKqCR_WFORYPM0vZQp-wXR2cJlz3oZTIeTmQmHeJHcsfCkWqaVXW7W0ryGfPlWTwqvpP93IS6OhzFfsIokKJHHqwPMn0gMtTlExv4hbBARj61lu06ERPSn9q_JceIWYbCtV-S3lRi-vlrr5QP86PL5R1vj2AmpKZwuqNOJIgU_E1MN-STeNVm7jgQBJysIg&X-OWA-CANARY=txPyjIj-l0yFi_bQxr_yo1CMrPEd5tsYu-jmTVj6TyAWjJyyPHJKNaYr5Z9p-uQavkiMXh-reE0.&owa=outlook.office.com&scriptVer=20231103003.19&animation=true"
-        alt="Jaume"
-        class="profile"
-      />
+      <iframe src="https://drive.google.com/file/d/1iZAozDqXdEV-Ar8_eVH5Y3JshIpm8uon/preview" class="profile" width="480" height="384px" allow="autoplay" ></iframe>
       <div class="container">
         <h2 class="name">Jaume</h2>
         <p class="title">Programmer &amp; Co-Founder</p>
@@ -95,11 +80,7 @@ function Home() {
 
       <div class="column">
       <div class="card"> 
-      <img
-        src="https://attachments.office.net/owa/jromeronaranjo%40stu.acs-schools.com/service.svc/s/GetAttachmentThumbnail?id=AQMkADdiYzU2NGExLWE5ODktNDFmMS05MjM3LTNkNDhmYzY4ZjFjYQBGAAAD2MdSUUL9ukiIY7OrKpo2xgcAQP4AhPDRhEmqRdWrl4UHrQAAAgEMAAAAQP4AhPDRhEmqRdWrl4UHrQACFXrbEAAAAAESABAAQpTU747i1EGTUEePkl9%2F0g%3D%3D&thumbnailType=2&token=eyJhbGciOiJSUzI1NiIsImtpZCI6IjczRkI5QkJFRjYzNjc4RDRGN0U4NEI0NDBCQUJCMTJBMzM5RDlGOTgiLCJ0eXAiOiJKV1QiLCJ4NXQiOiJjX3VidnZZMmVOVDM2RXRFQzZ1eEtqT2RuNWcifQ.eyJvcmlnaW4iOiJodHRwczovL291dGxvb2sub2ZmaWNlLmNvbSIsInVjIjoiY2Y1YTY2MWI0Y2YzNGEzNWE3ODc2NzY5Mzc5MWYxOWUiLCJzaWduaW5fc3RhdGUiOiJbXCJrbXNpXCJdIiwidmVyIjoiRXhjaGFuZ2UuQ2FsbGJhY2suVjEiLCJhcHBjdHhzZW5kZXIiOiJPd2FEb3dubG9hZEBiNDZkODFkMS1hMmUzLTQ5NjktOWRlZC04MTE1NDUyOTA4YjIiLCJpc3NyaW5nIjoiV1ciLCJhcHBjdHgiOiJ7XCJtc2V4Y2hwcm90XCI6XCJvd2FcIixcInB1aWRcIjpcIjExNTM4MDExMjAxNjIxNTU0MjdcIixcInNjb3BlXCI6XCJPd2FEb3dubG9hZFwiLFwib2lkXCI6XCJhNmZhNzczYS1lOTRjLTRhZDQtYTRmYy0yOTg4MjBmNTVjM2JcIixcInByaW1hcnlzaWRcIjpcIlMtMS01LTIxLTI0Mjc1MTU1NDktODQ0ODgzODgtMzY5OTAxNTgzNy0yNzkzMzQ1NlwifSIsIm5iZiI6MTcwMDA4MTk0NSwiZXhwIjoxNzAwMDgyNTQ1LCJpc3MiOiIwMDAwMDAwMi0wMDAwLTBmZjEtY2UwMC0wMDAwMDAwMDAwMDBAYjQ2ZDgxZDEtYTJlMy00OTY5LTlkZWQtODExNTQ1MjkwOGIyIiwiYXVkIjoiMDAwMDAwMDItMDAwMC0wZmYxLWNlMDAtMDAwMDAwMDAwMDAwL2F0dGFjaG1lbnRzLm9mZmljZS5uZXRAYjQ2ZDgxZDEtYTJlMy00OTY5LTlkZWQtODExNTQ1MjkwOGIyIiwiaGFwcCI6Im93YSJ9.KZlMI8TiTGEmMQU2i51j9ZPGrFw2oEtjwpu-cG_leOpbwugDedCG7LyDRVA-piA_0ac_rSPUkGeBnwjNlNAWuQp9_-txA6qZE3voZ29dZqebspcVg5mOO6DWFg_RacM-XBdhKH5ZTU_AP3JkQZkiwfdEKqCR_WFORYPM0vZQp-wXR2cJlz3oZTIeTmQmHeJHcsfCkWqaVXW7W0ryGfPlWTwqvpP93IS6OhzFfsIokKJHHqwPMn0gMtTlExv4hbBARj61lu06ERPSn9q_JceIWYbCtV-S3lRi-vlrr5QP86PL5R1vj2AmpKZwuqNOJIgU_E1MN-STeNVm7jgQBJysIg&X-OWA-CANARY=SmVtiGgOoEKUypz-VnO846Ac4rUd5tsYdBnT8H-Nu6DhbpWsOgVgRfJ20_CZlOqotXL9npweobk.&owa=outlook.office.com&scriptVer=20231103003.19&animation=true"
-        alt="Arkadii"
-        class="profile"
-      />
+      <iframe src="https://drive.google.com/file/d/1bMy6mLPLiUzP3OdZgzqEwQhLra5I3Jz-/preview" class="profile" width="480" height="384px" allow="autoplay" ></iframe>
       <div class="container">
         <h2 class="name">Arkadii</h2>
         <p class="title">Programmer &amp; Co-Founder</p>
@@ -112,11 +93,7 @@ function Home() {
 
       <div class="column">
       <div class="card"> 
-      <img
-        src="https://attachments.office.net/owa/jromeronaranjo%40stu.acs-schools.com/service.svc/s/GetAttachmentThumbnail?id=AQMkADdiYzU2NGExLWE5ODktNDFmMS05MjM3LTNkNDhmYzY4ZjFjYQBGAAAD2MdSUUL9ukiIY7OrKpo2xgcAQP4AhPDRhEmqRdWrl4UHrQAAAgEMAAAAQP4AhPDRhEmqRdWrl4UHrQACFXrbEAAAAAESABAAglnzSAA8NkKD8XdofD8Yrg%3D%3D&thumbnailType=2&token=eyJhbGciOiJSUzI1NiIsImtpZCI6IjczRkI5QkJFRjYzNjc4RDRGN0U4NEI0NDBCQUJCMTJBMzM5RDlGOTgiLCJ0eXAiOiJKV1QiLCJ4NXQiOiJjX3VidnZZMmVOVDM2RXRFQzZ1eEtqT2RuNWcifQ.eyJvcmlnaW4iOiJodHRwczovL291dGxvb2sub2ZmaWNlLmNvbSIsInVjIjoiY2Y1YTY2MWI0Y2YzNGEzNWE3ODc2NzY5Mzc5MWYxOWUiLCJzaWduaW5fc3RhdGUiOiJbXCJrbXNpXCJdIiwidmVyIjoiRXhjaGFuZ2UuQ2FsbGJhY2suVjEiLCJhcHBjdHhzZW5kZXIiOiJPd2FEb3dubG9hZEBiNDZkODFkMS1hMmUzLTQ5NjktOWRlZC04MTE1NDUyOTA4YjIiLCJpc3NyaW5nIjoiV1ciLCJhcHBjdHgiOiJ7XCJtc2V4Y2hwcm90XCI6XCJvd2FcIixcInB1aWRcIjpcIjExNTM4MDExMjAxNjIxNTU0MjdcIixcInNjb3BlXCI6XCJPd2FEb3dubG9hZFwiLFwib2lkXCI6XCJhNmZhNzczYS1lOTRjLTRhZDQtYTRmYy0yOTg4MjBmNTVjM2JcIixcInByaW1hcnlzaWRcIjpcIlMtMS01LTIxLTI0Mjc1MTU1NDktODQ0ODgzODgtMzY5OTAxNTgzNy0yNzkzMzQ1NlwifSIsIm5iZiI6MTcwMDA4MTk0NSwiZXhwIjoxNzAwMDgyNTQ1LCJpc3MiOiIwMDAwMDAwMi0wMDAwLTBmZjEtY2UwMC0wMDAwMDAwMDAwMDBAYjQ2ZDgxZDEtYTJlMy00OTY5LTlkZWQtODExNTQ1MjkwOGIyIiwiYXVkIjoiMDAwMDAwMDItMDAwMC0wZmYxLWNlMDAtMDAwMDAwMDAwMDAwL2F0dGFjaG1lbnRzLm9mZmljZS5uZXRAYjQ2ZDgxZDEtYTJlMy00OTY5LTlkZWQtODExNTQ1MjkwOGIyIiwiaGFwcCI6Im93YSJ9.KZlMI8TiTGEmMQU2i51j9ZPGrFw2oEtjwpu-cG_leOpbwugDedCG7LyDRVA-piA_0ac_rSPUkGeBnwjNlNAWuQp9_-txA6qZE3voZ29dZqebspcVg5mOO6DWFg_RacM-XBdhKH5ZTU_AP3JkQZkiwfdEKqCR_WFORYPM0vZQp-wXR2cJlz3oZTIeTmQmHeJHcsfCkWqaVXW7W0ryGfPlWTwqvpP93IS6OhzFfsIokKJHHqwPMn0gMtTlExv4hbBARj61lu06ERPSn9q_JceIWYbCtV-S3lRi-vlrr5QP86PL5R1vj2AmpKZwuqNOJIgU_E1MN-STeNVm7jgQBJysIg&X-OWA-CANARY=txPyjIj-l0yFi_bQxr_yo1CMrPEd5tsYu-jmTVj6TyAWjJyyPHJKNaYr5Z9p-uQavkiMXh-reE0.&owa=outlook.office.com&scriptVer=20231103003.19&animation=true"
-        alt="Max"
-        class="profile"
-      />
+      <iframe src="https://drive.google.com/file/d/1wdsoAvybBhRg1mkCltM7CPxMSxAAnhKJ/preview" class="profile" width="480" height="384px" allow="autoplay" ></iframe>
       <div class="container">
         <h2 class="name">Max</h2>
         <p class="title">Programmer &amp; Co-Founder</p>
