6b0db77f1586a8f9556cbae10f0a52134b0ff41f
UrName51 <98849399+UrName51@users.noreply.github.com>
2023-11-28 14:42:05 +0000

    footer

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "90d708d8d1477fea"; }
+/******/ 		__webpack_require__.h = function() { return "63ead232e1075a1d"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
