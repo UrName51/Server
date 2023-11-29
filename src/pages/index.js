480c0458f3ba37ea43c7c681b03ffa415222286c
UrName51 <98849399+UrName51@users.noreply.github.com>
2023-11-29 10:16:52 +0000

    github

--- src/pages/index.js
+++ src/pages/index.js
@@ -61,7 +61,10 @@ function Home() {
       <div class="card"> 
       <iframe src="https://drive.google.com/file/d/1sxlOQxq39ONdQiwct0_T5jlo6erLMBPU/preview" class="profile" width="304px" height="304px" allow="autoplay"></iframe>
       <div class="container">
-        <h2 class="name">Dhairya</h2>
+      <form class="profileTitle"> <h2 class="name">Dhairya</h2> <a HREF="https://github.com/TheVengeance7" class="github" > <svg height="24" aria-hidden="true" viewBox="0 0 16 16" version="1.1" width="24" data-view-component="true" fill="White" class="octicon octicon-mark-github">
+          <path d="M8 0c4.42 0 8 3.58 8 8a8.013 8.013 0 0 1-5.45 7.59c-.4.08-.55-.17-.55-.38 0-.27.01-1.13.01-2.2 0-.75-.25-1.23-.54-1.48 1.78-.2 3.65-.88 3.65-3.95 0-.88-.31-1.59-.82-2.15.08-.2.36-1.02-.08-2.12 0 0-.67-.22-2.2.82-.64-.18-1.32-.27-2-.27-.68 0-1.36.09-2 .27-1.53-1.03-2.2-.82-2.2-.82-.44 1.1-.16 1.92-.08 2.12-.51.56-.82 1.28-.82 2.15 0 3.06 1.86 3.75 3.64 3.95-.23.2-.44.55-.51 1.07-.46.21-1.61.55-2.33-.66-.15-.24-.6-.83-1.23-.82-.67.01-.27.38.01.53.34.19.73.9.82 1.13.16.45.68 1.31 2.69.94 0 .67.01 1.3.01 1.49 0 .21-.15.45-.55.38A7.995 7.995 0 0 1 0 8c0-4.42 3.58-8 8-8Z"></path>
+          </svg> </a>
+        </form>
         <p class="title">Developer &amp; Co-Founder</p>
         <p class="name">Some text that describes me lorem ipsum ipsum lorem.</p>
         <p class="name"> example@example.com</p>
@@ -75,7 +78,10 @@ function Home() {
       <div class="card"> 
       <iframe src="https://drive.google.com/file/d/1Eak-pfF6NyOgaxtE18HZKqIoIhfz0H5G/preview" class="profile" width="304px" height="304px" allow="autoplay"></iframe>
       <div class="container">
-        <h2 class="name">Emir</h2>
+      <form class="profileTitle"> <h2 class="name">Emir</h2> <a HREF="https://github.com/emirakbarov" class="github" > <svg height="24" aria-hidden="true" viewBox="0 0 16 16" version="1.1" width="24" data-view-component="true" fill="White" class="octicon octicon-mark-github">
+          <path d="M8 0c4.42 0 8 3.58 8 8a8.013 8.013 0 0 1-5.45 7.59c-.4.08-.55-.17-.55-.38 0-.27.01-1.13.01-2.2 0-.75-.25-1.23-.54-1.48 1.78-.2 3.65-.88 3.65-3.95 0-.88-.31-1.59-.82-2.15.08-.2.36-1.02-.08-2.12 0 0-.67-.22-2.2.82-.64-.18-1.32-.27-2-.27-.68 0-1.36.09-2 .27-1.53-1.03-2.2-.82-2.2-.82-.44 1.1-.16 1.92-.08 2.12-.51.56-.82 1.28-.82 2.15 0 3.06 1.86 3.75 3.64 3.95-.23.2-.44.55-.51 1.07-.46.21-1.61.55-2.33-.66-.15-.24-.6-.83-1.23-.82-.67.01-.27.38.01.53.34.19.73.9.82 1.13.16.45.68 1.31 2.69.94 0 .67.01 1.3.01 1.49 0 .21-.15.45-.55.38A7.995 7.995 0 0 1 0 8c0-4.42 3.58-8 8-8Z"></path>
+          </svg> </a>
+        </form>
         <p class="title">Developer &amp; Co-Founder</p>
         <p class="name">Some text that describes me lorem ipsum ipsum lorem.</p>
         <p class="name"> example@example.com</p>
@@ -88,7 +94,10 @@ function Home() {
       <div class="card"> 
       <iframe src="https://drive.google.com/file/d/1D-ekF2s4HKTaN9mELFHtui3cBbzUH9BB/preview" class="profile" width="304px" height="304px" allow="autoplay" ></iframe>
       <div class="container">
-        <h2 class="name">Shanay</h2>
+      <form class="profileTitle"> <h2 class="name">Shanay</h2> <a HREF="https://github.com/WulfTheWanderer" class="github" > <svg height="24" aria-hidden="true" viewBox="0 0 16 16" version="1.1" width="24" data-view-component="true" fill="White" class="octicon octicon-mark-github">
+          <path d="M8 0c4.42 0 8 3.58 8 8a8.013 8.013 0 0 1-5.45 7.59c-.4.08-.55-.17-.55-.38 0-.27.01-1.13.01-2.2 0-.75-.25-1.23-.54-1.48 1.78-.2 3.65-.88 3.65-3.95 0-.88-.31-1.59-.82-2.15.08-.2.36-1.02-.08-2.12 0 0-.67-.22-2.2.82-.64-.18-1.32-.27-2-.27-.68 0-1.36.09-2 .27-1.53-1.03-2.2-.82-2.2-.82-.44 1.1-.16 1.92-.08 2.12-.51.56-.82 1.28-.82 2.15 0 3.06 1.86 3.75 3.64 3.95-.23.2-.44.55-.51 1.07-.46.21-1.61.55-2.33-.66-.15-.24-.6-.83-1.23-.82-.67.01-.27.38.01.53.34.19.73.9.82 1.13.16.45.68 1.31 2.69.94 0 .67.01 1.3.01 1.49 0 .21-.15.45-.55.38A7.995 7.995 0 0 1 0 8c0-4.42 3.58-8 8-8Z"></path>
+          </svg> </a>
+        </form>
         <p class="title">Developer &amp; Co-Founder</p>
         <p class="name">Some text that describes me lorem ipsum ipsum lorem.</p>
         <p class="name"> example@example.com</p>
@@ -102,7 +111,10 @@ function Home() {
       <div class="card"> 
       <iframe src="https://drive.google.com/file/d/1iZAozDqXdEV-Ar8_eVH5Y3JshIpm8uon/preview" class="profile" width="304px" height="304px" allow="autoplay" ></iframe>
       <div class="container">
-        <h2 class="name">Jaume</h2>
+        <form class="profileTitle"> <h2 class="name">Jaume</h2> <a HREF="https://github.com/UrName51" class="github" > <svg height="24" aria-hidden="true" viewBox="0 0 16 16" version="1.1" width="24" data-view-component="true" fill="White" class="octicon octicon-mark-github">
+          <path d="M8 0c4.42 0 8 3.58 8 8a8.013 8.013 0 0 1-5.45 7.59c-.4.08-.55-.17-.55-.38 0-.27.01-1.13.01-2.2 0-.75-.25-1.23-.54-1.48 1.78-.2 3.65-.88 3.65-3.95 0-.88-.31-1.59-.82-2.15.08-.2.36-1.02-.08-2.12 0 0-.67-.22-2.2.82-.64-.18-1.32-.27-2-.27-.68 0-1.36.09-2 .27-1.53-1.03-2.2-.82-2.2-.82-.44 1.1-.16 1.92-.08 2.12-.51.56-.82 1.28-.82 2.15 0 3.06 1.86 3.75 3.64 3.95-.23.2-.44.55-.51 1.07-.46.21-1.61.55-2.33-.66-.15-.24-.6-.83-1.23-.82-.67.01-.27.38.01.53.34.19.73.9.82 1.13.16.45.68 1.31 2.69.94 0 .67.01 1.3.01 1.49 0 .21-.15.45-.55.38A7.995 7.995 0 0 1 0 8c0-4.42 3.58-8 8-8Z"></path>
+          </svg> </a>
+        </form>
         <p class="title">Developer &amp; Co-Founder</p>
         <p class="name">Some text that describes me lorem ipsum ipsum lorem.</p>
         <p class="name"> jaumern51@gmail.com</p>
@@ -115,7 +127,10 @@ function Home() {
       <div class="card"> 
       <iframe src="https://drive.google.com/file/d/1bMy6mLPLiUzP3OdZgzqEwQhLra5I3Jz-/preview" class="profile" width="304px" height="304px" allow="autoplay" ></iframe>
       <div class="container">
-        <h2 class="name">Arkadii</h2>
+      <form class="profileTitle"> <h2 class="name">Arkadii</h2> <a HREF="https://github.com/DenaryMantis" class="github" > <svg height="24" aria-hidden="true" viewBox="0 0 16 16" version="1.1" width="24" data-view-component="true" fill="White" class="octicon octicon-mark-github">
+          <path d="M8 0c4.42 0 8 3.58 8 8a8.013 8.013 0 0 1-5.45 7.59c-.4.08-.55-.17-.55-.38 0-.27.01-1.13.01-2.2 0-.75-.25-1.23-.54-1.48 1.78-.2 3.65-.88 3.65-3.95 0-.88-.31-1.59-.82-2.15.08-.2.36-1.02-.08-2.12 0 0-.67-.22-2.2.82-.64-.18-1.32-.27-2-.27-.68 0-1.36.09-2 .27-1.53-1.03-2.2-.82-2.2-.82-.44 1.1-.16 1.92-.08 2.12-.51.56-.82 1.28-.82 2.15 0 3.06 1.86 3.75 3.64 3.95-.23.2-.44.55-.51 1.07-.46.21-1.61.55-2.33-.66-.15-.24-.6-.83-1.23-.82-.67.01-.27.38.01.53.34.19.73.9.82 1.13.16.45.68 1.31 2.69.94 0 .67.01 1.3.01 1.49 0 .21-.15.45-.55.38A7.995 7.995 0 0 1 0 8c0-4.42 3.58-8 8-8Z"></path>
+          </svg> </a>
+        </form>
         <p class="title">Developer &amp; Co-Founder</p>
         <p class="name">Some text that describes me lorem ipsum ipsum lorem.</p>
         <p class="name"> example@example.com</p>
@@ -128,7 +143,10 @@ function Home() {
       <div class="card"> 
       <iframe src="https://drive.google.com/file/d/1wdsoAvybBhRg1mkCltM7CPxMSxAAnhKJ/preview" class="profile" width="304px" height="304px" allow="autoplay" ></iframe>
       <div class="container">
-        <h2 class="name">Maxence Finestone</h2>
+      <form class="profileTitle"> <h2 class="name">Arkadii</h2> <a HREF="https://github.com/MaxFinestone" class="github" > <svg height="24" aria-hidden="true" viewBox="0 0 16 16" version="1.1" width="24" data-view-component="true" fill="White" class="octicon octicon-mark-github">
+          <path d="M8 0c4.42 0 8 3.58 8 8a8.013 8.013 0 0 1-5.45 7.59c-.4.08-.55-.17-.55-.38 0-.27.01-1.13.01-2.2 0-.75-.25-1.23-.54-1.48 1.78-.2 3.65-.88 3.65-3.95 0-.88-.31-1.59-.82-2.15.08-.2.36-1.02-.08-2.12 0 0-.67-.22-2.2.82-.64-.18-1.32-.27-2-.27-.68 0-1.36.09-2 .27-1.53-1.03-2.2-.82-2.2-.82-.44 1.1-.16 1.92-.08 2.12-.51.56-.82 1.28-.82 2.15 0 3.06 1.86 3.75 3.64 3.95-.23.2-.44.55-.51 1.07-.46.21-1.61.55-2.33-.66-.15-.24-.6-.83-1.23-.82-.67.01-.27.38.01.53.34.19.73.9.82 1.13.16.45.68 1.31 2.69.94 0 .67.01 1.3.01 1.49 0 .21-.15.45-.55.38A7.995 7.995 0 0 1 0 8c0-4.42 3.58-8 8-8Z"></path>
+          </svg> </a>
+        </form>
         <p class="title">Developer &amp; Co-Founder</p>
         <p class="name">Some text that describes me lorem ipsum ipsum lorem.</p>
         <p class="name">finestonemax@gmail.com</p>
@@ -139,6 +157,7 @@ function Home() {
 
 
       </div>
+
       
 
       <footer class="footer">
